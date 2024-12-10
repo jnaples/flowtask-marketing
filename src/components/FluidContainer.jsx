9d541b24669124ff9a86245fa9a101b2f5e9b948
dojo <jnaples90@gmail.com>
2024-12-10 06:42:11 -0500

    Fix video section

--- src/components/FluidContainer.jsx
+++ src/components/FluidContainer.jsx
@@ -1,5 +1,10 @@
 import clsx from 'clsx'
 
 export function FluidContainer({ className, ...props }) {
-  return <div className={clsx('mx-auto max-w-7xl', className)} {...props} />
+  return (
+    <div
+      className={clsx('w-full px-4 sm:pl-6 lg:pl-8 lg:pr-0', className)}
+      {...props}
+    />
+  )
 }
--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import clsx from 'clsx'
 
-import { Container } from '@/components/Container'
 import backgroundImage from '../../public/background-features.jpg'
+import { FluidContainer } from './FluidContainer'
 
 const features = [
   {
@@ -67,7 +67,7 @@ export function PrimaryFeatures() {
         height={1636}
         unoptimized
       />
-      <Container className="relative">
+      <FluidContainer className="relative">
         <div className="flex max-w-3xl flex-col items-center text-center md:mx-auto">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
             The only tool you need to work smarter and achieve more
@@ -78,12 +78,12 @@ export function PrimaryFeatures() {
           </p>
         </div>
         <TabGroup
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
+          className="ml-auto mt-16 grid max-w-[95rem] grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
           vertical={tabOrientation === 'vertical'}
         >
           {({ selectedIndex }) => (
             <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
+              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:pb-0 md:px-6 lg:col-span-5 lg:px-0">
                 <TabList className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
                   {features.map((feature, featureIndex) => (
                     <div
@@ -131,13 +131,13 @@ export function PrimaryFeatures() {
                         {feature.description}
                       </p>
                     </div>
-                    <div className="relative mt-10 w-[45rem] overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                    <div className="relative mt-10 flex items-center justify-center rounded-4xl bg-slate-200 p-6 shadow-xl shadow-blue-900/20 sm:w-auto md:p-6 lg:mt-0 lg:h-[42rem] lg:rounded-br-none lg:rounded-tr-none lg:p-10 lg:pr-6 xl:p-14">
                       <video
                         className="w-full rounded-xl shadow-xl shadow-slate-900/10 ring-1 ring-slate-500/10"
                         src={feature.video}
                         alt={feature.title}
                         priority
-                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+                        sizes="100%"
                         autoPlay
                         muted
                         loop
@@ -150,7 +150,7 @@ export function PrimaryFeatures() {
             </>
           )}
         </TabGroup>
-      </Container>
+      </FluidContainer>
     </section>
   )
 }
