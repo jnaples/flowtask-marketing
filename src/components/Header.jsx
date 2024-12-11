bde0ab845f92e2516b8bd389ba5b978a104c6e98
Joe Naples <jnaples@zorustech.com>
2024-12-11 17:30:19 -0500

    Add log in to app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -70,7 +70,9 @@ function MobileNavigation() {
         <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
         <MobileNavLink href="#pricing">Pricing</MobileNavLink>
         <hr className="m-2 border-slate-300/40" />
-        <MobileNavLink href="/login">Sign in</MobileNavLink>
+        <MobileNavLink href="https://app.getflowtask.com/login">
+          Log in
+        </MobileNavLink>
       </PopoverPanel>
     </Popover>
   )
@@ -93,7 +95,7 @@ export function Header() {
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
             <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
+              <NavLink href="https://app.getflowtask.com/login">Log in</NavLink>
             </div>
             <Button href="#pricing" color="blue">
               <span>
