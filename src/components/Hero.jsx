7c96de0baa2a86d2ca199b80731e3fb070dede74
dojo <jnaples90@gmail.com>
2025-04-03 06:32:20 -0400

    Change hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,8 +12,8 @@ import logoTuple from '../../public/logos/tuple.svg'
 export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
-      <h1 className="mx-auto max-w-4xl font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Maximize productivity with{' '}
+      <h1 className="mx-auto max-w-5xl font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+        Maximize your productivity with{' '}
         <span className="relative whitespace-nowrap text-blue-600">
           <svg
             aria-hidden="true"
